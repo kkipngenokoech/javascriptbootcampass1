a866c6ef78f2eeb195d632b86a9748748ae5c7a8
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-06-22 19:40:28 +0300

    javascript bootcamp assignment one

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+3
+3+5
+console.log(3+5)
+let celcius =12
+let franfhreit=0
+franfhreit=celcius*1.2+36
+//console.log(franfhreit)
+//printing character ascii numbers
+let ascii_word="yes i AM a Fraud "
+for(let index=0;index<ascii_word.length;index++)
+{
+    console.log(ascii_word.charCodeAt(index))
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+
+    {/* document.getElementById("clickme").onclick= function()
+    {
+        displayname()
+    }  */}
+function displayname(name)
+{
+    document.write("hello, "+name+", welcome to the bootcamp");
+    // document.getElementById("clickme").innerHTML=("hello welcome to the bootcamp")
+}
